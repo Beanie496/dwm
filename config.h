6ac8076827ad2037664975c9cfa77a0d6c14a6c2
Jasper Shovelton <Beanie.github@proton.me>
2024-03-29 19:28:46 +0000

    Increase font size.

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means so bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Liberation Mono:size=8","Noto Color Emoji:size=8" };
-static const char dmenufont[]       = "monospace:size=8";
+static const char *fonts[]          = { "Liberation Mono:size=9","Noto Color Emoji:size=9" };
+static const char dmenufont[]       = "monospace:size=9";
 // static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 // static const char col_gray3[]       = "#bbbbbb";
