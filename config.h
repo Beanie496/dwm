efa9a6276ba8b8fc7db0d7435afb94a50b752b71
Jasper Shovelton <Beanie.github@proton.me>
2024-03-30 01:53:24 +0000

    Change dmenu font to be 'Liberation Mono'.

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means so bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Liberation Mono:size=9","Noto Color Emoji:size=9" };
-static const char dmenufont[]       = "monospace:size=9";
+static const char dmenufont[]       = "Liberation Mono:size=9";
 // static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 // static const char col_gray3[]       = "#bbbbbb";
