7113db1c90ee8f79fdace93ee15b69464bf8cee2
Jasper Shovelton <Beanie.github@proton.me>
2025-07-31 14:49:07 +0100

    Add extra keybindings from `gentoo-patches` branch.

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
-#include <X11/XF86keysym.h>
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Liberation Mono:size=9","Noto Color Emoji:size=9" };
+static const char *fonts[]          = { "Liberation Mono:size=9", "Noto Color Emoji:size=9" };
 static const char dmenufont[]       = "Liberation Mono:size=9";
 static const char col_gray[]        = "#222222";
 static const char col_purple[]      = "#c99fe8";
@@ -29,8 +29,6 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-// This allows the text in Vim in the master area to sit flush against the left
-// screen. It also gives a little over 80 chars for both master and slave.
 static const float mfact     = 65.0 / 128.0; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
@@ -60,76 +58,84 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
-	/* modifier                    key                function        argument */
-	{      0,                       XF86XK_AudioMute, spawn,          SHCMD("/usr/bin/wpctl set-mute @DEFAULT_SINK@ toggle; kill -36 $(pidof dwmblocks)") },
-	{      0,                XF86XK_AudioLowerVolume, spawn,          SHCMD("/usr/bin/wpctl set-volume @DEFAULT_SINK@ 5%-; kill -36 $(pidof dwmblocks)") },
-	{      0,                XF86XK_AudioRaiseVolume, spawn,          SHCMD("/usr/bin/wpctl set-volume @DEFAULT_SINK@ 5%+; kill -36 $(pidof dwmblocks)") },
-	{ ShiftMask,             XF86XK_AudioLowerVolume, spawn,          SHCMD("/usr/bin/wpctl set-volume @DEFAULT_SINK@ 1%-; kill -36 $(pidof dwmblocks)") },
-	{ ShiftMask,             XF86XK_AudioRaiseVolume, spawn,          SHCMD("/usr/bin/wpctl set-volume @DEFAULT_SINK@ 1%+; kill -36 $(pidof dwmblocks)") },
-	{ ControlMask,           XF86XK_AudioLowerVolume, spawn,          SHCMD("/usr/bin/wpctl set-volume @DEFAULT_SOURCE@ 5%-; kill -35 $(pidof dwmblocks)") },
-	{ ControlMask,           XF86XK_AudioRaiseVolume, spawn,          SHCMD("/usr/bin/wpctl set-volume @DEFAULT_SOURCE@ 5%+; kill -35 $(pidof dwmblocks)") },
-	{ ControlMask|ShiftMask, XF86XK_AudioLowerVolume, spawn,          SHCMD("/usr/bin/wpctl set-volume @DEFAULT_SOURCE@ 1%-; kill -35 $(pidof dwmblocks)") },
-	{ ControlMask|ShiftMask, XF86XK_AudioRaiseVolume, spawn,          SHCMD("/usr/bin/wpctl set-volume @DEFAULT_SOURCE@ 1%+; kill -35 $(pidof dwmblocks)") },
-	{      0,                    XF86XK_AudioMicMute, spawn,          SHCMD("/usr/bin/wpctl set-mute @DEFAULT_SOURCE@ toggle; kill -35 $(pidof dwmblocks)") },
-	{      0,               XF86XK_MonBrightnessDown, spawn,          SHCMD("/usr/bin/brightnessctl set 5%-; dwm_brightness") },
-	{      0,                 XF86XK_MonBrightnessUp, spawn,          SHCMD("/usr/bin/brightnessctl set 5%+; dwm_brightness") },
-	{ ShiftMask,            XF86XK_MonBrightnessDown, spawn,          SHCMD("/usr/bin/brightnessctl set 1%-; dwm_brightness") },
-	{ ShiftMask,              XF86XK_MonBrightnessUp, spawn,          SHCMD("/usr/bin/brightnessctl set 1%+; dwm_brightness") },
+	/* modifier               key                        function         argument */
+	{      0,                 XF86XK_AudioMute,          spawn,           SHCMD("wpctl set-mute @DEFAULT_SINK@ toggle; kill -36 $(pidof dwmblocks)") },
+	{      0,                 XF86XK_AudioLowerVolume,   spawn,           SHCMD("wpctl set-volume @DEFAULT_SINK@ 5%-; kill -36 $(pidof dwmblocks)") },
+	{ ShiftMask,              XF86XK_AudioLowerVolume,   spawn,           SHCMD("wpctl set-volume @DEFAULT_SINK@ 1%-; kill -36 $(pidof dwmblocks)") },
+	{ ControlMask,            XF86XK_AudioLowerVolume,   spawn,           SHCMD("wpctl set-volume @DEFAULT_SOURCE@ 5%-; kill -35 $(pidof dwmblocks)") },
+	{ ControlMask|ShiftMask,  XF86XK_AudioLowerVolume,   spawn,           SHCMD("wpctl set-volume @DEFAULT_SOURCE@ 1%-; kill -35 $(pidof dwmblocks)") },
+	{      0,                 XF86XK_AudioRaiseVolume,   spawn,           SHCMD("wpctl set-volume @DEFAULT_SINK@ 5%+; kill -36 $(pidof dwmblocks)") },
+	{ ShiftMask,              XF86XK_AudioRaiseVolume,   spawn,           SHCMD("wpctl set-volume @DEFAULT_SINK@ 1%+; kill -36 $(pidof dwmblocks)") },
+	{ ControlMask,            XF86XK_AudioRaiseVolume,   spawn,           SHCMD("wpctl set-volume @DEFAULT_SOURCE@ 5%+; kill -35 $(pidof dwmblocks)") },
+	{ ControlMask|ShiftMask,  XF86XK_AudioRaiseVolume,   spawn,           SHCMD("wpctl set-volume @DEFAULT_SOURCE@ 1%+; kill -35 $(pidof dwmblocks)") },
+	{      0,                 XF86XK_AudioMicMute,       spawn,           SHCMD("wpctl set-mute @DEFAULT_SOURCE@ toggle; kill -35 $(pidof dwmblocks)") },
+	{      0,                 XF86XK_MonBrightnessDown,  spawn,           SHCMD("brightnessctl set 5%-; dwm_brightness") },
+	{      0,                 XF86XK_MonBrightnessUp,    spawn,           SHCMD("brightnessctl set 5%+; dwm_brightness") },
+	{ ShiftMask,              XF86XK_MonBrightnessDown,  spawn,           SHCMD("brightnessctl set 1%-; dwm_brightness") },
+	{ ShiftMask,              XF86XK_MonBrightnessUp,    spawn,           SHCMD("brightnessctl set 1%+; dwm_brightness") },
 	// The snipping icon below PrtSc couldn't be found anywhere in
 	// /usr/include/X11/{keysymdef.h,keysym.h,XF86keysym.h}.
 	// {XK_Select,XF86XK_Copy,XF86XK_Cut,XF86XK_Select} - none worked.
 	// So for now, Control + PrtSc = select area for screenshot.
-	{ ControlMask,                 XK_Print,          spawn,          SHCMD("/usr/bin/scrot -s -fz '%Y-%m-%d_%H:%M:%S.png' -e 'mv $f ~/Pictures/Screenshots/'") },
-	{      0,                      XK_Print,          spawn,          SHCMD("/usr/bin/scrot -z '%Y-%m-%d_%H:%M:%S.png' -e 'mv $f ~/Pictures/Screenshots/'") },
-	{ MODKEY,                      XK_p,              spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,            XK_Return,         spawn,          {.v = termcmd } },
-	{ MODKEY,                      XK_b,              togglebar,      {0} },
-	{ MODKEY,                      XK_j,              focusstack,     {.i = +1 } },
-	{ MODKEY,                      XK_k,              focusstack,     {.i = -1 } },
-	{ MODKEY,                      XK_i,              incnmaster,     {.i = +1 } },
-	{ MODKEY,                      XK_d,              incnmaster,     {.i = -1 } },
-	{ MODKEY,                      XK_h,              setmfact,       {.f = -(1.0 / 64)} },
-	{ MODKEY,                      XK_l,              setmfact,       {.f = +(1.0 / 64)} },
-	{ MODKEY|ShiftMask,            XK_h,              setmfact,       {.f = -(1.0 / 128)} },
-	{ MODKEY|ShiftMask,            XK_l,              setmfact,       {.f = +(1.0 / 128)} },
-	{ MODKEY,                      XK_Return,         zoom,           {0} },
-	{ MODKEY,                      XK_Tab,            view,           {0} },
-	{ MODKEY|ShiftMask,            XK_c,              killclient,     {0} },
-	{ MODKEY,                      XK_t,              setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                      XK_f,              setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                      XK_m,              setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                      XK_space,          setlayout,      {0} },
-	{ MODKEY|ShiftMask,            XK_space,          togglefloating, {0} },
-	{ MODKEY|ShiftMask,            XK_0,              tag,            {.ui = ~0 } },
-	{ MODKEY,                      XK_comma,          focusmon,       {.i = -1 } },
-	{ MODKEY,                      XK_period,         focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,            XK_comma,          tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,            XK_period,         tagmon,         {.i = +1 } },
-	{ MODKEY|ShiftMask,            XK_q,              quit,           {0} },
-	TAGKEYS(                       XK_1,                               0)
-	TAGKEYS(                       XK_2,                               1)
-	TAGKEYS(                       XK_3,                               2)
-	TAGKEYS(                       XK_4,                               3)
-	TAGKEYS(                       XK_5,                               4)
-	TAGKEYS(                       XK_6,                               5)
-	TAGKEYS(                       XK_7,                               6)
-	TAGKEYS(                       XK_8,                               7)
-	TAGKEYS(                       XK_9,                               8)
+	{ ControlMask,            XK_Print,                  spawn,           SHCMD("scrot -s -fz '%Y-%m-%d_%H:%M:%S.png' -e 'mv $f ~/Pictures/Screenshots/'") },
+	{      0,                 XK_Print,                  spawn,           SHCMD("scrot -z '%Y-%m-%d_%H:%M:%S.png' -e 'mv $f ~/Pictures/Screenshots/'") },
+	{ MODKEY,                 XK_Tab,                    view,            {0} },
+	{ MODKEY,                 XK_q,                      quit,            {0} },
+	{ MODKEY,                 XK_t,                      setlayout,       {.v = &layouts[0]} },
+	{ MODKEY|ShiftMask,       XK_t,                      spawn,           SHCMD("thunderbird") },
+	{ MODKEY,                 XK_i,                      incnmaster,      {.i = +1 } },
+	{ MODKEY,                 XK_o,                      spawn,           SHCMD("obsidian") },
+	{ MODKEY|ShiftMask,       XK_o,                      spawn,           SHCMD("libreoffice") },
+	{ MODKEY,                 XK_p,                      spawn,           {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,       XK_p,                      spawn,           SHCMD("sh $BROWSER") },
+	{ MODKEY,                 XK_Return,                 spawn,           {.v = termcmd } },
+	{ MODKEY|ShiftMask,       XK_Return,                 zoom,            {0} },
+	{ MODKEY,                 XK_a,                      spawn,           SHCMD("audacity") },
+	{ MODKEY,                 XK_s,                      spawn,           SHCMD("spotify") },
+	{ MODKEY|ShiftMask,       XK_s,                      spawn,           SHCMD("steam") },
+	{ MODKEY,                 XK_d,                      incnmaster,      {.i = -1 } },
+	{ MODKEY,                 XK_f,                      setlayout,       {.v = &layouts[1]} },
+	{ MODKEY,                 XK_g,                      spawn,           SHCMD("gimp") },
+	{ MODKEY,                 XK_h,                      setmfact,        {.f = -(1.0 / 64)} },
+	{ MODKEY|ShiftMask,       XK_h,                      setmfact,        {.f = -(1.0 / 128)} },
+	{ MODKEY,                 XK_j,                      focusstack,      {.i = +1 } },
+	{ MODKEY,                 XK_k,                      focusstack,      {.i = -1 } },
+	{ MODKEY,                 XK_l,                      setmfact,        {.f = +(1.0 / 64)} },
+	{ MODKEY|ShiftMask,       XK_l,                      setmfact,        {.f = +(1.0 / 128)} },
+	{ MODKEY,                 XK_z,                      spawn,           SHCMD("zathura") },
+	{ MODKEY,                 XK_c,                      killclient,      {0} },
+	{ MODKEY,                 XK_b,                      togglebar,       {0} },
+	{ MODKEY,                 XK_m,                      setlayout,       {.v = &layouts[2]} },
+	{ MODKEY,                 XK_comma,                  focusmon,        {.i = -1 } },
+	{ MODKEY|ShiftMask,       XK_comma,                  tagmon,          {.i = -1 } },
+	{ MODKEY,                 XK_period,                 focusmon,        {.i = +1 } },
+	{ MODKEY|ShiftMask,       XK_period,                 tagmon,          {.i = +1 } },
+	{ MODKEY,                 XK_space,                  setlayout,       {0} },
+	{ MODKEY|ShiftMask,       XK_space,                  togglefloating,  {0} },
+	TAGKEYS(                  XK_1,                                        0)
+	TAGKEYS(                  XK_2,                                        1)
+	TAGKEYS(                  XK_3,                                        2)
+	TAGKEYS(                  XK_4,                                        3)
+	TAGKEYS(                  XK_5,                                        4)
+	TAGKEYS(                  XK_6,                                        5)
+	TAGKEYS(                  XK_7,                                        6)
+	TAGKEYS(                  XK_8,                                        7)
+	TAGKEYS(                  XK_9,                                        8)
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
-	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	/* click                event mask      button          function         argument */
+	{ ClkLtSymbol,          0,              Button1,        setlayout,       {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,       {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,              Button2,        zoom,            {0} },
+	{ ClkStatusText,        0,              Button2,        spawn,           {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,       {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating,  {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,     {0} },
+	{ ClkTagBar,            0,              Button1,        view,            {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,      {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,             {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,       {0} },
 };
